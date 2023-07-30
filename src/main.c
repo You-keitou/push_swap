b21320add0b51a2a3dd1d4abc0a35924bb6a5dbc
You-Keitou <youkeitou327@gmail.com>
2023-07-31 03:14:46 +0900

    6個以下のソート実装

--- src/main.c
+++ src/main.c
@@ -6,11 +6,23 @@
 /*   By: jinyang <jinyang@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 19:56:29 by jinyang           #+#    #+#             */
-/*   Updated: 2023/07/31 02:49:24 by jinyang          ###   ########.fr       */
+/*   Updated: 2023/07/31 03:14:15 by jinyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	print_all(t_list **stack)
+{
+	t_list *tmp;
+	tmp = *stack;
+	ft_printf("############\n");
+	while (tmp->content)
+	{
+		ft_printf("%d\n", *(tmp->content));
+		tmp = tmp->next;
+	}
+}
 
 static void	free_stack(t_list **stack_a, t_list **stack_b)
 {
@@ -46,9 +58,11 @@ int	main(int argc, char **argv)
 			return (0);
 		cord_compress(array, argc - 1);
 		init_stack(stack_a, stack_b, array, argc - 1);
-		// sort(stack_a, stack_b, argc - 1);
+		sort(stack_a, stack_b, argc - 1);
+		print_all(stack_a);
+		print_all(stack_b);
 		free(array);
-		free_stack(stack_a, stack_a);
+		free_stack(stack_a, stack_b);
 	}
 	return (0);
 }
--- src/sort.c
+++ src/sort.c
@@ -6,9 +6,67 @@
 /*   By: jinyang <jinyang@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 22:30:27 by jinyang           #+#    #+#             */
-/*   Updated: 2023/07/31 02:46:49 by jinyang          ###   ########.fr       */
+/*   Updated: 2023/07/31 03:01:24 by jinyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void sort_three(t_list **stack_a)
+{
+	int first;
+	int second;
+	int third;
+
+    first = *((*(stack_a))->content);
+    second = *(((*(stack_a))->next)->content);
+    third = *(((*(stack_a))->next->next)->content);
+	if (first > second && second < third && third > first)
+		sa(stack_a);
+	else if (first > second && second > third && third < first)
+	{
+		sa(stack_a);
+		rra(stack_a);
+	}
+	else if (first > second && second < third && third < first)
+        ra(stack_a);
+    else if (first < second && second > third && third > first)
+    {
+		sa(stack_a);
+		ra(stack_a);
+	}
+    else if (first < second && second > third && third < first)
+		rra(stack_a);
+}
+
+void	sort_under_six(t_list **stack_a, t_list **stack_b)
+{
+	int stack_head_num;
+    int will_push_num;
+
+    will_push_num = 0;
+    while (ft_lstsize(*stack_a) > 3)
+    {
+        stack_head_num = *((*(stack_a))->content);
+        if (will_push_num == stack_head_num)
+        {
+			pb(stack_a, stack_b);
+			will_push_num++;
+		}
+        else
+			ra(stack_a);
+	}
+	sort_three(stack_a);
+    while (ft_lstsize(*stack_b) > 0)
+		pa(stack_a, stack_b);
+}
+
+void sort(t_list **stack_a, t_list **stack_b, int size)
+{
+	if (size == 2)
+		sa(stack_a);
+	else if (size == 3)
+		sort_three(stack_a);
+	else if (size <= 6)
+		sort_under_six(stack_a, stack_b);
+}
